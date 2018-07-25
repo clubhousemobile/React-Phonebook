b7c7fdfd801009e9b919db293e05fc26121b20cd
Node Player <nodeplayer@Nodes-iMac.local>
2018-07-25 06:28:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm'
+import PhoneInfoList from './components/PhoneInfoList'
+
+class App extends Component {
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: 'Player',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: 'Player111',
+        phone: '010-0000-0011'
+      }
+    ],
+    keyword: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    })
+  }
+
+  handleCreate = (data) => {
+    const {information} = this.state;
+    this.setState({
+      information:information.concat({id:this.id++, ...data})
+    })
+  }
+  handleRemove = (id) =>{
+    const {information} = this.state;
+    this.setState({
+      information:information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) =>{
+    const {information} = this.state;
+    this.setState({
+      information:information.map(
+        info => info.id === id ? {...info, ...data} : info
+      )
+    })
+  }
+
+  render() {
+
+    const {information, keyword} = this.state;
+    
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
+
+    return (
+      <div>
+        <PhoneForm 
+          onCreate={this.handleCreate}
+        />
+        <p>
+          <input 
+            placeholder="Seach names..."
+            onChange={this.handleChange}
+            value={keyword}
+          />
+        </p>
+        <PhoneInfoList 
+          data={filteredList}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
